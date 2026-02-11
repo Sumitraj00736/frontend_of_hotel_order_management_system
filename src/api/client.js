077d52bd019c5b4a5194ba33d1fc24b0302e57bd
Sumit Raj <sumitraj00736@gmail.com>
2026-02-11 21:27:19 +0545

    change backend url

--- src/api/client.js
+++ src/api/client.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { getToken } from './session.js';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:4000'
+  baseURL:  'http://localhost:4000'
 });
 
 api.interceptors.request.use((config) => {
