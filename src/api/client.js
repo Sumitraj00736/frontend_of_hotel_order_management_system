257841d32f9d1436103f6cf630d6114d45c3540a
Sumit Raj <sumitraj00736@gmail.com>
2026-02-11 21:31:24 +0545

    fix

--- src/api/client.js
+++ src/api/client.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { getToken } from './session.js';
 
 const api = axios.create({
-  baseURL:  'http://localhost:4000'
+  baseURL: 'https://hotel-order-management-system.onrender.com'
 });
 
 api.interceptors.request.use((config) => {
