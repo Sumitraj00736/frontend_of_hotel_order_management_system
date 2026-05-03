d70670c8348fb9eb2b58c07e9fcc7aa1de0c5537
Sanjana Yadav <126410251+SanjanaYdv1@users.noreply.github.com>
2026-05-03 20:04:23 +0545

    updated on orders for admin

--- src/components/admin/orders/create/CustomizeDishModal.jsx
+++ src/components/admin/orders/create/CustomizeDishModal.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import '../../../../common/css/admin/orders/CustomizeDishModal.css';
 
 const CustomizeDishModal = ({ open, item, onClose, onAdd }) => {
   const [qty, setQty] = useState(1);
