dbba33a48072ed7496adc95b575b0189c51301f7
Sanjana Yadav <126410251+SanjanaYdv1@users.noreply.github.com>
2026-05-08 20:34:44 +0545

    updated menu

--- src/components/admin/orders/create/MenuSection.jsx
+++ src/components/admin/orders/create/MenuSection.jsx
@@ -1,9 +1,6 @@
 import React, { useState } from 'react';
 import { LayoutGrid, Search, Star, Plus, X, ChevronRight } from 'lucide-react';
 import '../../../../common/css/admin/orders/menuSection.css';
-import React from 'react';
-import { LayoutGrid, Search, Star, Heart, Plus } from 'lucide-react';
-import '../../../../common/css/admin/orders/MenuSection.css';
 
 
 const MenuSection = ({
