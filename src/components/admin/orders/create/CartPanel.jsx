68e29e4bb708741f28a2154c8d73f9cda009bf7b
Sumit Raj <sumitraj00736@gmail.com>
2026-05-09 09:36:55 +0545

    refactor: update CartPanel import path to reference OrderItemsPremium stylesheet

--- src/components/admin/orders/create/CartPanel.jsx
+++ src/components/admin/orders/create/CartPanel.jsx
@@ -3,7 +3,7 @@ import {
   User, Users, ReceiptText, Trash2, ChevronRight, 
   Minus, Plus, ChevronDown, ShoppingBag 
 } from 'lucide-react';
-import '../../../../common/css/admin/orders/AddItemsModal.css';
+import '../../../../common/css/admin/orders/OrderItemsPremium.css';
 
 const CartPanel = ({
   items = [],
