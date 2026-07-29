201c30c497ca2d63c8fa85f22ef6bccf92e64aa3
Sumit Raj <sumitraj00736@gmail.com>
2026-07-29 17:56:35 +0545

    refactor: remove bootstrap dependency and add database password placeholder to environment configuration

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './styles/app.css';
 import './customcss/table.css';
 import './customcss/form.css';
